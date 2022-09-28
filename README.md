a5a1a0f37d9ae1611bd5c953d55ce06de357ffba
Shanief Webb <slw07g@users.noreply.github.com>
2022-09-28 14:08:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Chrome Search Engine Importer
+
+Custom Search Engines in chrome can make searching for things in private/internal systems a lot more efficient and easier, so long as those systems allow you to input the search string in the URI.
+
+Custom Search Engines can also function as keyword shortcuts to URLs, if you don't wish to fumble around with your bookmarks and would rather type in a keyword in the search bar + <Enter> to take you to where you are trying to go.
+
+However, when you collaborate with other people who often use the same systems that you do, it can be helpful to share these custom search gines with one another. The https://zapier.com/blog/add-search-engine-to-chrome/ for adding custom search engines does not scale well when you need to share a lot of them. Thus, the need for this script to make that manual process easier and the framework of defining custom search engine keywords as YAML files.
+
+# How does it work?
+
+## Defining search engine keywords in YAML
+
+You will need to create a YML file with your hsortcuts like the example below. The example file, `example_search_engines.yml` also has some practical examples.
+
+```
+- name: Shortcut 1 Name
+  keyword: shorcut1keyword
+  url: https://shortcut1/url?q={searchTerms}
+- name: Shortcut 2 Name
+  keyword: shortcut2keyword
+  url: https://shortcut2url.com/search/%s
+- name: Virustotal UI
+  keyword: vtui
+  url: https://www.virustotal.com
+```
+
+NOTE: `%s` and `{searchTerms}` are synonymous. All occurrences of `%s` in the URL are replaced with `{searchTerms}` when being added to the database.
+
+## Using the script
+
+Once you have defined your shortcuts, you may use the script, you must pass the path to the YML file to it. Example:
+
+`python3 chrome-search-engine-importer.py -e example_search_engines.yml`
+
+By default, the script will look in the default paths for Chrome profiles (on MacOS, Windows, and Linux). You will be given the option to choose which profiles found in the default paths to load the keyhwords in.
+
+To bypass this, you may optionally use the `-p` flag to specify a path to a profile that is not in a default path.  
+_NOTE: The path should point to the directory that has the sqlite database file `Web Data` inside of it._
+
+```
+> python3 chrome-search-engine-importer.py --help
+Usage: chrome-search-engine-importer.py [OPTIONS]
+
+Options:
+  -e, --search-engines-file TEXT  Path to a YAML file with search engines
+                                  defined  [required]
+  -p, --profile TEXT              Path to a chrome profile. Useful if a target
+                                  chrome profile is in a custom path.
+  --help                          Show this message and exit.
+```
+
+The script will install any keywords in the YAML that do not already exist in the `Web Data` database. If you want to add these to be added, either adjust modify the keyword so that it is unique, or delete the existing keyword from your chrome search engines [here](chrome://settings/searchEngines).
+
+For the newly added keywords to be visible in chrome, you must relaunch Chrome so that it reloads the SQLite database. I recommend enabling the "Continue where you left off" [Startup Option](chrome://settings/onStartup) in Chrome so that your tabs recover after you restart the browser.
---
+++ chrome-search-engine-importer.py
@@ -0,0 +1,128 @@
+import logging
+import os
+import sqlite3
+import sys
+import shutil
+import click
+import inquirer
+import yaml
+
+logging.getLogger().setLevel(logging.INFO)
+
+# Default paths for where chrome profiles can be found as subdirectories.
+DEFAULT_CHROME_PATHS = {
+    'darwin': ['~/Library/Application Support/Google/Chrome/'],
+    'win32': ['%USERPROFILE%\\AppData\\Local\\Google\\Chrome\\User Data'],
+    'linux': ['~/.config/google-chrome/']
+}
+
+TMP_WEB_DATA_PATH = '~/.tmp_web_data'
+
+
+def copy_file(src: str, dst: str) -> None:
+    logging.info("Copying %s to %s", src, dst)
+    shutil.copyfile(expand_path(src), expand_path(dst))
+    os.sync()
+
+
+def rm_file(path: str) -> None:
+    logging.info("Deleting %ss", path)
+    os.remove(expand_path(path))
+
+
+def expand_path(path: str) -> str:
+    return os.path.abspath(os.path.normpath(os.path.expanduser(path)))
+
+
+def check_keyword_exists(con: sqlite3.Cursor, keyword: str) -> bool:
+    cursor = con.execute('SELECT * FROM keywords WHERE keyword = ?', [keyword])
+    results = cursor.fetchall()
+    return len(results) > 0
+
+
+def install_search_engines(target: str, engines: list = None) -> bool:
+    print(engines)
+    web_data_original = os.path.join(target, 'Web Data')
+    copy_file(web_data_original, TMP_WEB_DATA_PATH)
+    con = sqlite3.connect(expand_path(TMP_WEB_DATA_PATH))
+    profile_name = os.path.basename(target)
+
+    for engine in engines:
+        keyword = engine['keyword']
+        name = engine['name']
+        url = engine['url']
+        if not check_keyword_exists(con, engine['keyword']):
+            logging.info(
+                'Adding keyword [%s] to profile [%s]',
+                keyword,
+                profile_name,
+            )
+            con.execute(
+                'INSERT INTO keywords(short_name, keyword, url, favicon_url) values (?, ?, ?, "");',
+                [name, keyword, url])
+            con.commit()
+        else:
+            logging.info(
+                'Keyword [%s] already exists in profile [%s]...skipping...',
+                keyword,
+                profile_name,
+            )
+    con.close()
+    copy_file(TMP_WEB_DATA_PATH, web_data_original)
+    rm_file(TMP_WEB_DATA_PATH)
+
+
+def read_search_engines(path: str) -> list:
+    logging.info("Loading search engines from %s", path)
+    return yaml.safe_load(open(expand_path(path), 'r'))
+
+
+def get_chrome_profile_paths() -> list:
+    profile_paths = []
+    logging.info('Searching for chrome profiles in default paths')
+    for path in DEFAULT_CHROME_PATHS.get(sys.platform, []):
+        for root, _, _ in os.walk(expand_path(path)):
+            if os.path.exists(os.path.join(
+                    root, 'Web Data')) and os.path.basename(root) != 'System Profile':
+                profile_paths.append(root)
+            continue
+    return profile_paths
+
+
+def get_target_profiles(profile_paths: list) -> list:
+    prompts = [
+        inquirer.Checkbox(
+            'profiles',
+            message="Which Chrome profiles should the search engines be installed on? (You may select more than one)",
+            choices=profile_paths,
+            default=profile_paths[0] if len(profile_paths) > 0 else None),
+    ]
+
+    answers = inquirer.prompt(prompts)
+    return answers.get('profiles', [])
+
+
+@click.command()
+@click.option(
+    '--search-engines-file',
+    '-e',
+    help="Path to a YAML file with search engines defined",
+    type=str,
+    required=True)
+@click.option(
+    '--profile',
+    '-p',
+    help="Path to a chrome profile. Useful if a target chrome profile is in a custom path.",
+    type=str,
+    required=False)
+def main(search_engines_file: str = None, profile: str = None):
+    profile_paths = get_chrome_profile_paths() if not profile else profile.split(',')
+    targets = get_target_profiles(profile_paths)
+    engines = read_search_engines(search_engines_file)
+    for target in targets:
+        print(target)
+        install_search_engines(target, engines=engines)
+
+
+if __name__ == '__main__':
+    main()
